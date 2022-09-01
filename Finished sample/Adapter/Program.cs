70ce57552b6e57eee4c1f14bfdbd537793e4f819
jwellington58 <wj070996@gmail.com>
2022-08-31 21:12:31 -0300

    REmoving finished solution

--- Finished sample/Adapter/Program.cs
+++
@@ -1,9 +0,0 @@
-﻿using Adapter;
-using Adapter.ClassAdapter;
-
-// adapter example (same code for object & class adapter)
-ICityAdapter adapter = new CityAdapter();
-var city = adapter.GetCity();
-
-Console.WriteLine($"{city.FullName}, {city.Inhabitants}");
-Console.ReadKey(); 
--- Finished sample/Strategy/Program.cs
+++
@@ -1,5 +0,0 @@
-﻿using Strategy;
-
-var order = new Order("Marvin Software", 5, "Visual Studio License");
-order.Export(new CSVExportService());
-order.Export(new JsonExportService());
