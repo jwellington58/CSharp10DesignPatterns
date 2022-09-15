f959a3193ea9994551c1f3ddda0fdf9df6ed7218
jwellington58 <wj070996@gmail.com>
2022-09-14 22:02:01 -0300

    Adding Discount Factory

---
+++ Starter solution/FactoryMethod/CodeDiscountFactory.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FactoryMethod
+{
+    /// <summary>
+    /// Concrete creator
+    /// </summary>
+    public class CodeDiscountFactory : DiscountFactory
+    {
+        private readonly string _code;
+
+        public CodeDiscountFactory(string code)
+        {
+            _code = code;
+        }
+
+        public override DiscountService CreateDiscountService() 
+            => new CodeDiscountService(_code);
+    }
+}
---
+++ Starter solution/FactoryMethod/CountryDiscountFactory.cs
@@ -0,0 +1,18 @@
+﻿namespace FactoryMethod
+{
+    /// <summary>
+    /// Concrete creator
+    /// </summary>
+    public class CountryDiscountFactory : DiscountFactory
+    {
+        private readonly string _countryKeyWord;
+
+        public CountryDiscountFactory(string countryKeyWord)
+        {
+            _countryKeyWord = countryKeyWord;
+        }
+
+        public override DiscountService CreateDiscountService()
+            => new CountryDiscountService(_countryKeyWord);
+    }
+}
